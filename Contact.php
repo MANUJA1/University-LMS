d70c56d6e0f1654f0a9571aa5f9c53927c309f22
MANUJA1 <manujamapalagama@gmail.com>
2016-08-03 09:54:26 -0700

    contact us

---
+++ Contact.php
@@ -0,0 +1,144 @@
+<?php require 'functions/connection.php'; ?>
+<?php require 'includes/header.php'; ?>
+<?php require 'includes/body.php'; ?>
+<link href="css/jombo.css" type="text/css" rel="stylesheet">
+
+<title> View digree</title>
+
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        
+            <div class="parallax overflow-hidden bg-blue-400 page-section third">
+        <div class="container parallax-layer" data-opacity="true">
+            <div class="media v-middle">
+                <div class="media-left text-center">
+                    <a href="#">
+                        <img src="img/pic1.jpg" alt="people" class="img-circle width-60" />
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="text-white text-display-1 margin-v-0">Sanka Rajapaksha</h4>
+                    <h6 class="text-subhead"><a class="link-white text-underline" href="website-student-public-profile.html">View public profile</a></h6>
+                </div>
+                <div class="media-right">
+                    <h class="label bg-blue-500"></h>
+                    
+                </div>
+            </div>
+        </div>
+    </div> </div>
+    </div>
+<body>
+    <div class="jumbotron">
+        <h1>Contact Us</h1>
+        <p>Join with us and succsess your furure....</p>
+
+    </div>
+
+
+
+    <div class="container">
+        <div class="row">
+
+            <div class="row">
+            <div class="col-md-6">
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="sizing-addon1">User Name  &nbsp    </span>
+                    <input type="text" class="form-control" placeholder="Type hear" aria-describedby="sizing-addon1">
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="sizing-addon1">  E-mail  &nbsp &nbsp &nbsp &nbsp</span>
+                    <input type="text" class="form-control" placeholder="Type hear" aria-describedby="sizing-addon1">
+
+
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="sizing-addon1">Contact No </span>
+                    <input type="text" class="form-control" placeholder="Type hear" aria-describedby="sizing-addon1">
+
+
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="form-group col-lg-6">
+            <label for="comment"><h3>Comment:</h3></label>
+            <textarea class="form-control" rows="10" id="comment"></textarea>
+            <br>
+            <button type="button" class="btn btn-primary btn-block">SEND </button>
+
+        </div>
+    </div>
+        <div class="col-md-12">
+            <div class="map">
+                <img src="img/map.PNG"alt="simely fafe" align="right"  width="500" height="400" align="top">
+            </div>
+        </div>
+         <br><br><br><br><br>
+
+
+        <div class="jumbotron">
+        <div class="col-md-4">
+        <div class="form-group col-lg-60">
+        <address><p>School of Management</p>
+            <strong>Twitter, Inc.</strong><br>
+            1355 Market Street, Suite 900<br>
+            San Francisco, CA 94103<br>
+            <abbr title="Phone">P:</abbr> (123) 456-7890
+        </address>
+
+
+        <address>
+            <strong>Full Name</strong><br>
+            <a href="mailto:#">first.last@example.com</a>
+        </address>
+            </div></div>
+
+        <div class="col-md-4">
+            <div class="form-group col-lg-60">
+                <address><p>School of Management</p>
+                    <strong>Twitter, Inc.</strong><br>
+                    1355 Market Street, Suite 900<br>
+                    San Francisco, CA 94103<br>
+                    <abbr title="Phone">P:</abbr> (123) 456-7890
+                </address>
+
+
+                <address>
+                    <strong>Full Name</strong><br>
+                    <a href="mailto:#">first.last@example.com</a>
+                </address>
+            </div></div>
+
+        <div class="col-md-4">
+            <div class="form-group col-lg-60">
+                <address><p>School of Management</p>
+                    <strong>Twitter, Inc.</strong><br>
+                    1355 Market Street, Suite 900<br>
+                    San Francisco, CA 94103<br>
+                    <abbr title="Phone">P:</abbr> (123) 456-7890
+                </address>
+
+                <address>
+                    <strong>Full Name</strong><br>
+                    <a href="mailto:#">first.last@example.com</a>
+                </address>
+            </div></div>
+
+</body>
+
+<?php require 'includes/footer.php'; ?>
