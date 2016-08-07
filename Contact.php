1b874557593935aec450b071ce6558c135b1e2ea
MANUJA1 <manujamapalagama@gmail.com>
2016-08-07 06:54:02 -0700

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,170 @@
+<?php require 'functions/connection.php'; ?>
+<?php require 'includes/header.php'; ?>
+<?php require 'includes/body.php'; ?>
+<link href="css/jombo.css" type="text/css" rel="stylesheet">
+
+<title> View digree</title>
+
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        
+            <div class="parallax overflow-hidden bg-blue-400 page-section third">
+        <div class="container parallax-layer" data-opacity="true">
+            <div class="media v-middle">
+                <div class="media-left text-center">
+                    <a href="#">
+                        <img src="img/pic1.jpg" alt="people" class="img-circle width-60" />
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="text-white text-display-1 margin-v-0">Sanka Rajapaksha</h4>
+                    <h6 class="text-subhead"><a class="link-white text-underline" href="website-student-public-profile.html">View public profile</a></h6>
+                </div>
+                <div class="media-right">
+                    <h class="label bg-blue-500"></h>
+                    
+                </div>
+            </div>
+        </div>
+    </div> </div>
+    </div>
+    <body background="yt2.png">
+    <div class="jumbotron"align="middle">
+
+        <h1>Contact Us</h1>
+        <p>Join with us and succsess your furure....</p>
+    </div>
+
+
+
+    <div class="container">
+        <div class="row">
+
+            <div class="row">
+            <div class="col-md-6">
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="sizing-addon1">User Name&nbsp </span>
+                    <input type="text" class="form-control" placeholder="Type hear" aria-describedby="sizing-addon1">
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="sizing-addon1">  E-mail  &nbsp &nbsp &nbsp &nbsp</span>
+                    <input type="text" class="form-control" placeholder="Type hear" aria-describedby="sizing-addon1">
+
+
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="sizing-addon1">Contact No </span>
+                    <input type="text" class="form-control" placeholder="Type hear" aria-describedby="sizing-addon1">
+
+
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="form-group col-lg-6">
+            <label for="comment"><h3>Comment:</h3></label>
+            <textarea class="form-control" rows="10" id="comment"></textarea>
+            <br>
+            <button type="button" class="btn btn-primary btn-block">SEND </button>
+
+        </div>
+    </div>
+        <div class="col-md-12.1">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63385.14306866688!2d80.02269518862062!3d6.
+            821860889124286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae252771cac42c3%3A0x20bb96f0f2f48572!
+            2sNSBM+Green+University!5e0!3m2!1sen!2s!4v1470537260588"
+                    width="1142" height="400"frameborder="0" style="border:0" allowfullscreen></iframe>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                  width="600" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+
+
+
+        <div class="jumbotron">
+        <div class="col-md-4">
+        <div class="form-group col-lg-60">
+        <address><p>School of Management</p>
+            <strong>Address.</strong><br>
+            309, High Level Road<br>
+            Colombo 05<br>
+            00500<br>
+            Sri Lanka<br>
+
+            <abbr title="Phone">P:</abbr> +94 (11) 544 5000<br>
+            <abbr title="Fax">F:</abbr>+94 (11) 544 5009
+        </address>
+
+
+        <address>
+            <strong>Full Name</strong><br>
+            <a href="mailto:#">www.nsbm.lk/</a>
+        </address>
+            </div></div>
+
+        <div class="col-md-4">
+            <div class="form-group col-lg-60">
+                <address><p>School of Computing</p>
+                    <strong>Twitter, Inc.</strong><br>
+                    318, High Level Road<br>
+                    Colombo 05<br>
+                    00500<br>
+                    Sri Lanka<br>
+                    <abbr title="Phone">P:</abbr> +94 (11) 544 6000
+                </address>
+
+
+                <address>
+                    <strong>Full Name</strong><br>
+                    <a href="mailto:#">www.nsbm.lk/</a>
+                </address>
+            </div></div>
+
+        <div class="col-md-4">
+            <div class="form-group col-lg-60">
+                <address><p>NSBM Exam Unit</p>
+                    <strong>Address</strong><br>
+                    309, High Level Road<br>
+                    Colombo 05<br>
+                    00500<br>
+                    Sri Lanka<br>
+                    <abbr title="Phone">P:</abbr> +94 (11) 544 5004
+                </address>
+
+                <address>
+                    <strong>Full Name</strong><br>
+                    <a href="mailto:#">www.nsbm.lk/</a>
+                </address>
+            </div></div>
+
+</body>
+
+    <div id="foot2">
+        <table border="0">
+            <tr>
+                <th>Contact Us On</th></tr>
+                <td><a href="#"><img src="map.PNG" width="20" height="20">
+                                <img src="Images/tw2.png" width="20" height="20"><a href="899.html">
+                                <img src="Images/yt2.png" width="20" height="20"></td></tr><a href="899.html">
+                <td></td></tr>
+
+                <td></td>
+                <td></td></tr>
+        </table>
+
+
+    </div>
+
+<?php require 'includes/footer.php'; ?>
